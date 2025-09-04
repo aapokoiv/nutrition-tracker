a0b14d2e22960cd6d0e136a1cb0a8b9ac1331e3d
aapokoiv <koivula.aapo@gmail.com>
2025-09-04 13:43:52 +0300

    Added static and templates directories and experimenting with flask

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    foods = ["peruna", "banaani", "kanapihvi"]
+    return render_template("index.html", message="Welcome!", foods=foods)
